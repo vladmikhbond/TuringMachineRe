9a3393a8bf9d9f288ba8fb4f3ebe5c8d4add6e26
volodymyr bondariev <vladmikhbond@hotmail.com>
2019-08-11 21:44:25 +0300

    init

--- src/App.js
+++ src/App.js
@@ -5,9 +5,23 @@ import Turing from './model.js';
 
 export default class App extends React.Component
 {
+  constructor(props) {
+    super(props);
+    this.state = {input: 'aaa', program: '' };
+    this.tm = null;
+  }
+
+  inputChangeHandler(e) {
+    this.setState({input: e.target.value.trim()});
+  }
+
+  areaChangedHandler (text) {
+    this.setState({program: text.trim()})
+  }
+
 
   init() {
-    // tm = new Turing(rulesText.value.trim(), input.value.trim());
+     this.tm = new Turing(this.state.program, this.state.input);
     // printer.innerHTML = "";
     // if (timer) {
     //   clearInterval(timer);
@@ -21,9 +35,9 @@ export default class App extends React.Component
        <table>
          <tr>
            <td>
-             <Area />
+             <Area onChange={this.areaChangedHandler.bind(this)}/>
              <div>
-               <input id="input" value="aaa" />
+               <input value={this.state.input} onChange={this.inputChangeHandler.bind(this)}/>
              </div>
              <div>
                <button id="runButton" onClick={this.runClick}> >></button>
--- src/Area.js
+++ src/Area.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import './App.css';
-import Turing from './model.js';
+
+const sample = `sa = sbR
+s. = s.STOP`;
 
 export default class Area extends React.Component
 {
-   render() {
+    constructor(props) {
+        super(props);
+        this.state = {value: sample};
+        this.handleChange = this.handleChange.bind(this);
+        this.props.onChange(sample);
+    }
+
+    handleChange(event) {
+        const text = event.target.value;
+        this.setState({value: text});
+        this.props.onChange(text);
+    }
+
+    render() {
        return (
-           <textarea id="rulesText">sa = sbR
-s. = s.STOP
-        </textarea>
+           <textarea value={this.state.value} onChange={this.handleChange}></textarea>
        )
    }
 }
